773821ab74e12d33e8ceb6fc61360cd17e30c43d
GedMarc <marc.magon@gmail.com>
2021-09-29 22:26:06 +0200

    hibernate jrt reader updates

--- hibernate-core/src/jre11/java/module-info.java
+++ hibernate-core/src/jre11/java/module-info.java
@@ -1,7 +1,4 @@
 module org.hibernate.orm.core.shade {
-	requires jakarta.persistence;
-//	requires com.guicedee.guicedinjection;
-	
 	requires static org.hibernate.orm.core;
 	requires org.jboss.logging;
 }
--- hibernate-core/src/jre11/java/org/hibernate/boot/archive/internal/shade/JarFileBasedArchiveDescriptor.java
+++ hibernate-core/src/jre11/java/org/hibernate/boot/archive/internal/shade/JarFileBasedArchiveDescriptor.java
@@ -1,263 +1,148 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
 package org.hibernate.boot.archive.internal.shade;
-
-
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Enumeration;
-import java.util.jar.JarFile;
-import java.util.jar.JarInputStream;
-import java.util.zip.ZipEntry;
 
 import org.hibernate.boot.archive.spi.AbstractArchiveDescriptor;
 import org.hibernate.boot.archive.spi.ArchiveContext;
 import org.hibernate.boot.archive.spi.ArchiveDescriptorFactory;
 import org.hibernate.boot.archive.spi.ArchiveEntry;
 import org.hibernate.boot.archive.spi.ArchiveEntryHandler;
-import org.hibernate.boot.archive.spi.ArchiveException;
 import org.hibernate.boot.archive.spi.InputStreamAccess;
 
-import static org.hibernate.internal.log.UrlMessageBundle.URL_LOGGER;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import org.jboss.logging.Logger;
 
 /**
- * An ArchiveDescriptor implementation leveraging the {@link java.util.jar.JarFile} API for processing.
+ * An ArchiveDescriptor implementation leveraging the {@link java.util.jar.JarFile} API for processing - specifically meant to support the new URL format for JRT and modules
  *
  * @author Steve Ebersole
+ * @author Marc Magon
  */
-public class JarFileBasedArchiveDescriptor extends AbstractArchiveDescriptor {
+public class JarFileBasedArchiveDescriptor extends AbstractArchiveDescriptor
+{
+	private static final Logger log = Logger.getLogger( JarFileBasedArchiveDescriptor.class );
+	
+	private ArchiveContext context;
+	
 	/**
 	 * Constructs a JarFileBasedArchiveDescriptor
 	 *
 	 * @param archiveDescriptorFactory The factory creating this
-	 * @param archiveUrl The url to the JAR file
-	 * @param entry The prefix for entries within the JAR url
+	 * @param archiveUrl               The url to the JRT Module VFS file
+	 * @param entry                    The prefix for entries within the JRT VFS url
 	 */
 	public JarFileBasedArchiveDescriptor(
 			ArchiveDescriptorFactory archiveDescriptorFactory,
 			URL archiveUrl,
-			String entry) {
-		super( archiveDescriptorFactory, archiveUrl, entry );
-	}
-
-	@Override
-	public void visitArchive(ArchiveContext context) {
-		if(getArchiveUrl().toString().startsWith("jrt:/"))
-		{
-			visitRuntime(context);
-			return;
-		}
-
-		final JarFile jarFile = resolveJarFileReference();
-		if ( jarFile == null ) {
-			return;
-		}
-
-		try {
-			final Enumeration<? extends ZipEntry> zipEntries = jarFile.entries();
-			while ( zipEntries.hasMoreElements() ) {
-				final ZipEntry zipEntry = zipEntries.nextElement();
-				final String entryName = extractName( zipEntry );
-
-				if ( getEntryBasePrefix() != null && ! entryName.startsWith( getEntryBasePrefix() ) ) {
-					continue;
-				}
-				if ( zipEntry.isDirectory() ) {
-					continue;
-				}
-
-				if ( entryName.equals( getEntryBasePrefix() ) ) {
-					// exact match, might be a nested jar entry (ie from jar:file:..../foo.ear!/bar.jar)
-					//
-					// This algorithm assumes that the zipped file is only the URL root (including entry), not
-					// just any random entry
-					try (	final InputStream is = new BufferedInputStream( jarFile.getInputStream( zipEntry ) );
-					         final JarInputStream jarInputStream = new JarInputStream( is )) {
-						ZipEntry subZipEntry = jarInputStream.getNextEntry();
-						while ( subZipEntry != null ) {
-							if ( ! subZipEntry.isDirectory() ) {
-
-								final String name = extractName( subZipEntry );
-								final String relativeName = extractRelativeName( subZipEntry );
-								final InputStreamAccess inputStreamAccess = buildByteBasedInputStreamAccess( name, jarInputStream );
-
-								final ArchiveEntry entry = new ArchiveEntry() {
-									@Override
-									public String getName() {
-										return name;
-									}
-
-									@Override
-									public String getNameWithinArchive() {
-										return relativeName;
-									}
-
-									@Override
-									public InputStreamAccess getStreamAccess() {
-										return inputStreamAccess;
-									}
-								};
-
-								final ArchiveEntryHandler entryHandler = context.obtainArchiveEntryHandler( entry );
-								entryHandler.handleEntry( entry, context );
-							}
-
-							subZipEntry = jarInputStream.getNextEntry();
-						}
-					}
-					catch (Exception e) {
-						throw new RuntimeException( "Error accessing JarFile entry [" + zipEntry.getName() + "]", e );
-					}
-				}
-				else {
-					final String name = extractName( zipEntry );
-					final String relativeName = extractRelativeName( zipEntry );
-					final InputStreamAccess inputStreamAccess;
-					try (InputStream is = jarFile.getInputStream( zipEntry )) {
-						inputStreamAccess = buildByteBasedInputStreamAccess( name, is );
-					}
-					catch (IOException e) {
-						throw new RuntimeException(
-								String.format(
-										"Unable to access stream from jar file [%s] for entry [%s]",
-										jarFile.getName(),
-										zipEntry.getName()
-								             )
-						);
-					}
-
-					final ArchiveEntry entry = new ArchiveEntry() {
-						@Override
-						public String getName() {
-							return name;
-						}
-
-						@Override
-						public String getNameWithinArchive() {
-							return relativeName;
-						}
-
-						@Override
-						public InputStreamAccess getStreamAccess() {
-							return inputStreamAccess;
-						}
-					};
-
-					final ArchiveEntryHandler entryHandler = context.obtainArchiveEntryHandler( entry );
-					entryHandler.handleEntry( entry, context );
-				}
-			}
-		}
-		finally {
-			try {
-				jarFile.close();
-			}
-			catch ( Exception ignore ) {
-			}
-		}
-	}
-
-	private JarFile resolveJarFileReference() {
-		try {
-			final String filePart = getArchiveUrl().getFile();
-			if ( filePart != null && filePart.indexOf( ' ' ) != -1 ) {
-				// unescaped (from the container), keep as is
-				return new JarFile( getArchiveUrl().getFile() );
-			}
-			else {
-				return new JarFile( getArchiveUrl().toURI().getSchemeSpecificPart() );
-			}
-		}
-		catch (IOException e) {
-			URL_LOGGER.logUnableToFindFileByUrl( getArchiveUrl(), e );
-		}
-		catch (URISyntaxException e) {
-			URL_LOGGER.logMalformedUrl( getArchiveUrl(), e );
-		}
-		return null;
-	}
-
-
-	public void visitRuntime(ArchiveContext context)
+			String entry)
 	{
-		final String jrtPart = getArchiveUrl().getPath();
+		super(archiveDescriptorFactory, archiveUrl, entry);
+	}
+	
+	/**
+	 * Visits a JRT virtual file system using the JDK 8 nio file mechanisms,
+	 *
+	 * @param context
+	 */
+	@Override
+	public void visitArchive(ArchiveContext context)
+	{
+		this.context = context;
+		java.nio.file.Path path = null;
 		try
 		{
-			Path path = Paths.get(getArchiveUrl().toURI());
-			String basePrefix = getEntryBasePrefix();
-			String name = path.toString();
-			name = name.startsWith("/modules/") ? name.substring(9) : name;
-			Files.walkFileTree(path, new SimpleFileVisitor<>(){
-				@Override
-				public FileVisitResult visitFile(Path a, BasicFileAttributes attrs) throws IOException
-				{
-					String name = a.toString();
-					if(name.startsWith("/modules/"))
-					{
-						name = name.substring(9);
-						name = name.substring(name.indexOf('/') + 1);
-					}
-					
-					final String entryName = name;
-					final String relativeName = basePrefix != null && name.contains(basePrefix)
-							? name.substring(basePrefix.length())
-							: name;
-					final InputStreamAccess inputStreamAccess;
-					try (InputStream is = Files.newInputStream(a))
-					{
-						inputStreamAccess = buildByteBasedInputStreamAccess(name, is);
-					}
-					catch (Exception e)
-					{
-						System.out.println("exception in walk - ");
-						e.printStackTrace();
-						throw new RuntimeException(
-								String.format(
-										"Unable to access stream from jrt ref [%s] for entry [%s]",
-										path,
-										a.toString()
-								)
-						);
-					}
-					final ArchiveEntry entry = new ArchiveEntry()
-					{
-						@Override
-						public String getName()
-						{
-							return entryName;
-						}
-						
-						@Override
-						public String getNameWithinArchive()
-						{
-							return relativeName;
-						}
-						
-						@Override
-						public InputStreamAccess getStreamAccess()
-						{
-							return inputStreamAccess;
-						}
-					};
-					
-					final ArchiveEntryHandler entryHandler = context.obtainArchiveEntryHandler(entry);
-					entryHandler.handleEntry(entry, context);
-					return super.visitFile(a, attrs);
-				}
-			});
+			path = java.nio.file.Paths.get(getArchiveUrl().toURI());
+			java.nio.file.Files.walkFileTree(path, new JRTModuleWalker());
 		}
 		catch (URISyntaxException | IOException e)
 		{
-			throw new RuntimeException(
-					String.format(
-							"Unable to access stream from jrt location [%s]",
-							jrtPart)
+			throw new IllegalArgumentException(
+					"Unable to visit JRT / Path " + path + ". Cause: " + e.getMessage(), e
 			);
 		}
-
+	}
+	
+	/**
+	 * Peruses through the modular file system, finding anything applicable in modules that have opened access to the module org.hibernate.orm (this)
+	 * Ignores modules that have not exposed and/or opened to our module
+	 */
+	protected class JRTModuleWalker extends java.nio.file.SimpleFileVisitor<java.nio.file.Path>
+	{
+		@Override
+		public java.nio.file.FileVisitResult visitFile(java.nio.file.Path path, java.nio.file.attribute.BasicFileAttributes attrs) throws java.io.IOException
+		{
+			String name = path.toString();
+			if (name.startsWith("/modules/"))
+			{
+				name = name.substring(9);
+				name = name.substring(name.indexOf('/') + 1);
+			}
+			java.nio.file.Path lateralPath = null;
+			try
+			{
+				lateralPath = java.nio.file.Paths.get(getArchiveUrl().toURI());
+			}
+			catch (java.net.URISyntaxException e)
+			{
+				visitFileFailed(path, new java.io.IOException("Unable to walk file", e));
+			}
+			String basePrefix = getEntryBasePrefix();
+			final String entryName = name;
+			final String relativeName = basePrefix != null && name.contains(basePrefix)
+					? name.substring(basePrefix.length())
+					: name;
+			final InputStreamAccess inputStreamAccess;
+			try (InputStream is = java.nio.file.Files.newInputStream(lateralPath))
+			{
+				inputStreamAccess = buildByteBasedInputStreamAccess(name, is);
+			}
+			catch (Exception e)
+			{
+				throw new IOException(
+						String.format(
+								"Unable to access stream from jrt ref [%s] for entry [%s]",
+								lateralPath,
+								lateralPath.toString()
+						)
+				);
+			}
+			final ArchiveEntry entry = new ArchiveEntry()
+			{
+				@Override
+				public String getName()
+				{
+					return entryName;
+				}
+				
+				@Override
+				public String getNameWithinArchive()
+				{
+					return relativeName;
+				}
+				
+				@Override
+				public InputStreamAccess getStreamAccess()
+				{
+					return inputStreamAccess;
+				}
+			};
+			
+			final ArchiveEntryHandler entryHandler = context.obtainArchiveEntryHandler(entry);
+			entryHandler.handleEntry(entry, context);
+			return super.visitFile(lateralPath, attrs);
+		}
+		
+		@Override
+		public java.nio.file.FileVisitResult visitFileFailed(java.nio.file.Path file, java.io.IOException exc) throws java.io.IOException
+		{
+			return super.visitFileFailed(file, exc);
+		}
 	}
 }
