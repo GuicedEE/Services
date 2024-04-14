c5e76b5337baf08aec8351b23cecda617c87f56c
GedMarc <marc.magon@gmail.com>
2024-04-14 05:03:21 +0200

    Configure JUL logging to default to SLF4J

--- guice-inject-client/src/main/java/com/guicedee/client/implementations/GuicedEEClientStartup.java
+++ guice-inject-client/src/main/java/com/guicedee/client/implementations/GuicedEEClientStartup.java
@@ -1,6 +1,9 @@
 package com.guicedee.client.implementations;
 
 import com.guicedee.guicedinjection.interfaces.IGuicePreStartup;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
+import java.util.logging.LogManager;
 
 public class GuicedEEClientStartup implements IGuicePreStartup<GuicedEEClientStartup>
 {
@@ -8,5 +11,8 @@ public class GuicedEEClientStartup implements IGuicePreStartup<GuicedEEClientSta
     public void onStartup()
     {
         System.setProperty("org.jboss.logging.provider", "slf4j");
+        LogManager.getLogManager()
+                  .getLogger("")
+                  .addHandler(new SLF4JBridgeHandler());
     }
 }
