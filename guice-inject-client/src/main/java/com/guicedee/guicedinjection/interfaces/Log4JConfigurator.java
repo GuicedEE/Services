2c317195527ef3a4e757f2272a0a595d8642a662
GedMarc <marc.magon@gmail.com>
2025-03-02 21:43:49 +0200

    Group startup items, and use Future's

--- guice-inject-client/src/main/java/com/guicedee/guicedinjection/interfaces/Log4JConfigurator.java
+++ guice-inject-client/src/main/java/com/guicedee/guicedinjection/interfaces/Log4JConfigurator.java
@@ -1,10 +1,11 @@
 package com.guicedee.guicedinjection.interfaces;
 
+import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.Configurator;
 import org.apache.logging.log4j.core.config.builder.api.*;
 import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
 
 public interface Log4JConfigurator
 {
-    ConfigurationBuilder<BuiltConfiguration> configure(ConfigurationBuilder<BuiltConfiguration> builder,RootLoggerComponentBuilder rootLoggerComponentBuilder);
+    void configure(Configuration config);
 }
